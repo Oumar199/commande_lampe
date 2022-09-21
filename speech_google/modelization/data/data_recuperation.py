db6c33996ca72d684a46e2fea474c3a27bf8f9f0
= <=>
2022-09-21 10:14:57 +0000

    recuperation de donnees

---
+++ speech_google/modelization/data/data_recuperation.py
@@ -0,0 +1,24 @@
+"""Module de récupération de données avec transformations.
+"""
+import torch
+from torch.utils.data import Dataset
+from torchvision.datasets import ImageFolder
+from typing import Union, List
+from torchvision.transforms import Compose
+import os
+
+def get_spectrograms(path: str, transformers: Compose):
+    """Récupération des spectrogrammes avec ImageFolder de pytorch
+
+    Args:
+        path (str): Chemin vers les spectrogrammes   
+        transformers (Compose): Les transformations a effectuées sur les spectrogrammes
+    """
+    if os.path.exists(path):
+        dataset = ImageFolder(path, transform=transformers)
+        return dataset
+    else:
+        raise OSError("Le chemin spécifié est introuvale !")
+    
+    
+    
